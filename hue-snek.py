ead8824d51c5ac8c0beb4277b0a8d084cb6bfda4
channel-42 <hallowmd@gmail.com>
2020-03-24 15:28:39 +0100

    WIP

--- hue-snek.py
+++ hue-snek.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 '''
 Made as a "practice-project" with the help of Phue by  Nathanaël Lécaudé
 A small Hue Api Library for python
@@ -14,12 +13,12 @@ import urllib3
 
 http = urllib3.PoolManager()
 
-class Light(object):
 
+class Light(object):
     def __init__(self, light_id, bridge):
         self.bridge = bridge
         self.light_id = light_id
-        
+
         self.name
         self.brightness
         self.saturation
@@ -27,15 +26,15 @@ class Light(object):
         self.state
 
     def __repr__(self):
-        #change how isinstances are represented 
+        #change how isinstances are represented
         return f'<{self.__class__.__module__}.{self.__class__.__name__} object "{self.name}"'
-            
+
     def get(self):
         #linking to get_light functio for internal class use
         return self.bridge.get_light(self.light_id)
 
-class hue(object):
 
+class hue(object):
     def __init__(self, ip=None, user=None):
         self.ip = ip
         self.user = user
@@ -44,34 +43,38 @@ class hue(object):
         self.light_name = {}
         self.light_modelid = {}
 
-    def checkup(self):      #basic up check and username check
+    def checkup(self):  #basic up check and username check
         api = http.request('GET', self.address)
         #check for http connection
         if api.status != 200:
             return 1
-        
+
         js = json.loads(api.data.decode('utf-8'))
-        #check for api error 
+        #check for api error
         try:
             if js[0]['error']:
                 return js[0]['error']['description']
         except:
             pass
             return 0
-    
+
     def req(self, mode='GET', address=None, data=None):
         #main request function for GET and PUT
         if address == None:
             address = self.address
         try:
-            
+
             if mode == 'GET':
-                ret = json.loads(http.request(mode, address).data.decode('utf-8)'))
-                return dict(ret) 
-        
+                ret = json.loads(
+                    http.request(mode, address).data.decode('utf-8)'))
+                return dict(ret)
+
             if (mode == 'POST') or (mode == 'PUT'):
-                http.request(mode, address, body=data, headers={'Content-Type': 'application/json'})
-                return 0 
+                http.request(mode,
+                             address,
+                             body=data,
+                             headers={'Content-Type': 'application/json'})
+                return 0
         except:
             return 1
 
@@ -82,22 +85,22 @@ class hue(object):
 
         lid = str(light_id)
         state = self.req('GET', f'{self.address}lights/{lid}')
-        
+
         if param is None:
             return state
-        
+
         if param in ['name', 'modelid', 'type', 'productname', 'productid']:
             return state[param]
 
     def get_lights(self, mode=None):
         #gets all lights (WIP: CHANGE TO LIGHT OBJECT INSTEAD OF LIST!!)
         lights = self.req('GET', f'{self.address}lights')
-        
+
         for light, val in lights.items():
             self.light_id[int(light)] = light
             self.light_name[int(light)] = val['name']
             self.light_modelid[int(light)] = val['modelid']
-        
+
         if mode == "id":
             return self.light_id
         if mode == "name":
@@ -105,6 +108,8 @@ class hue(object):
         if mode == "modelid":
             return self.light_modelid
 
-hue("http://192.168.178.75", "O4qAaBl9LaXonrNlAu0Pzei3ianWAJuUzYuZpC2I").get_light(1, 'productid')
+
+hue("http://192.168.178.75",
+    "O4qAaBl9LaXonrNlAu0Pzei3ianWAJuUzYuZpC2I").get_light(1, 'productid')
 
 #print(hue("http://192.168.178.75", "O4qAaBl9LaXonrNlAu0Pzei3ianWAJuUzYuZpC2I").req('GET', 'http://192.168.178.75/api/O4qAaBl9LaXonrNlAu0Pzei3ianWAJuUzYuZpC2I/lights/1'))

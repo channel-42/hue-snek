ca60085982cfab4f57758c80e98f07b5f1a4748b
channel-42 <hallowmd@gmail.com>
2020-03-25 16:45:58 +0100

    Updating readme and WIP

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# hue-snek [WIP]
+# hue_snek [WIP]
 > A python Library for the Philips Hue API
 
 <img align="left" border="0" padding="4" src="https://github.com/channel-42/hue-snek/blob/master/.resources/snek.png" width="50%">
 
 # About
-hue-snek is a python library for the Philips Hue API, that allows for easy API integration into other projects.
+hue_snek is a python library for the Philips Hue API, that allows for easy API integration into other projects.
 
 I will add more documentation and progress reports in the following days since the projects is still very bare bones at the moment.
 
@@ -13,14 +13,43 @@ I will add more documentation and progress reports in the following days since t
 - get light information (name, state, modelname, etc.)
 - set light parameter (on/off, brightness, hue)
 - connect to bridge
+- 
+
 
 # WIP
 
 - add parameter control
 - basically finish most of the base code: Light class, hue class functions
+- Light class properties
+-
 
-# Examples
+# Example
 
+Basic usage of this library.
+
+> Note that at the moment the hue_snek.py file has to be in the same directory as your script or in pythons path. You could also append the system path at the beginning of you file with `import sys` and then `sys.path.append('path/to/file')`, then `import hue_snek`.
+
+```python
+
+from hue_snek import hue, Light
+
+#setup bridge
+h = hue('192.168.178.20', 'generic-username')
+
+#to check the connection to the bridge use:
+h.checkup()  #returns 0 if connection and username OK
+
+#to get Light information use either the Light or Hue class
+
+#Light
+Light(1, h).name
+Light(1, h).brightness
+
+#Hue
+h.get_light(2, 'name')  #parameters: see hue api
+h.get_lights('id')  #modes: obj (standard), id, name, modelid
+
+```
 # Notes
 
 This project was inspired by ![Phue](https://github.com/studioimaginaire/phue/). Check it out :)
--- hue_snek.py
+++ hue_snek.py
@@ -3,6 +3,9 @@
 Made as a "practice-project" with the help of Phue by  Nathanaël Lécaudé
 A small Hue Api Library for python
 
+Light class:
+    - missing properties: saturation, hue, state (on/off)
+
 hue-snek By Channel-42
 '''
 
@@ -11,6 +14,7 @@ import os
 import sys
 import urllib3
 
+#urllib Pool Manager is needed for http requests
 http = urllib3.PoolManager()
 
 
@@ -26,7 +30,7 @@ class Light(object):
         self._state = None
 
     def __repr__(self):
-        #change how isinstances are represented
+        #change how instances are represented
         return f'<{self.__class__.__module__}.{self.__class__.__name__} object "{self.name}"'
 
     def get(self, *args):
@@ -37,6 +41,8 @@ class Light(object):
         #link to set_light function for internal class use
         return self.bridge.set_light(self.light_id, *args)
 
+    #get and set properties of light object (name, brightness, etc.)
+
     @property
     def name(self):
         return self.get('name')
@@ -44,6 +50,17 @@ class Light(object):
     @name.setter
     def name(self, value):
         self._name = value
+        self.set('name', self._name)
+
+    @property
+    def brightness(self):
+        self._brightness = self.get('bri')
+        return self._brightness
+
+    @brightness.setter
+    def brightness(self, value):
+        self._brightness = value
+        self.set('bri', self._brightness)
 
 
 class hue(object):
@@ -54,6 +71,7 @@ class hue(object):
         self.light_id = {}
         self.light_name = {}
         self.light_modelid = {}
+        self.light_object = {}
 
     def checkup(self):  #basic up check and username check
         api = http.request('GET', self.address)
@@ -62,7 +80,7 @@ class hue(object):
             return 1
 
         js = json.loads(api.data.decode('utf-8'))
-        #check for api error
+        #check for api error (eg wrong username)
         try:
             if js[0]['error']:
                 return js[0]['error']['description']
@@ -73,14 +91,14 @@ class hue(object):
     def req(self, mode='GET', address=None, data=None):
         #main request function for GET and PUT
         if address == None:
-            address = self.address
+            address = self.address  #use standard adress (see __init__)
         try:
-
+            #get request
             if mode == 'GET':
                 ret = json.loads(
                     http.request(mode, address).data.decode('utf-8)'))
                 return dict(ret)
-
+            #put request (body and header needed for urllib3)
             if (mode == 'POST') or (mode == 'PUT'):
                 http.request(mode,
                              address,
@@ -95,34 +113,40 @@ class hue(object):
         if light_id is None:
             return 1
 
-        lid = str(light_id)
-        state = self.req('GET', f'{self.address}lights/{lid}')
+        lid = str(light_id)  #make sure light id is string
+        state = self.req('GET', f'{self.address}lights/{lid}')  #make request
 
         if param is None:
-            return state
+            return state  #return whole light state page
 
+        #check if parameter is in main body or in state subbody (see json structure)
         if param in ['name', 'modelid', 'type', 'productname', 'productid']:
             return state[param]
+        else:
+            return state['state'][param]
 
     def set_light(self, light_id=None, param=None, value=None):
-        #set light's parameter value based on light id
+        #set light's parameter value based on light id using the req function
         if (light_id is None) or (param is None) or (value is None):
             return 1
-        data = f'{{"{param}":{value}}}'
-        print(data)
-        lid = str(light_id)
-        self.req('PUT', f'{self.address}lights/{lid}/state', data)
-        print(f'{self.address}lights/{lid}/state')
 
-    def get_lights(self, mode=None):
-        #gets all lights (WIP: CHANGE TO LIGHT OBJECT INSTEAD OF LIST!!)
+        data = f'{{"{param}":{value}}}'
+        lid = str(light_id)  #make sure light_id is string
+        self.req('PUT', f'{self.address}lights/{lid}/state', data)
+        return 0
+
+    def get_lights(self, mode='obj'):
+        #gets all lights based on selected mode (standard is object mode) and returns dict
         lights = self.req('GET', f'{self.address}lights')
 
         for light, val in lights.items():
-            self.light_id[int(light)] = light
+            self.light_object[int(light)] = Light(int(light), self)
+            self.light_id[int(light)] = Light(int(light), self).light_id
             self.light_name[int(light)] = val['name']
             self.light_modelid[int(light)] = val['modelid']
 
+        if mode == 'obj':
+            return self.light_object
         if mode == "id":
             return self.light_id
         if mode == "name":
@@ -131,4 +155,6 @@ class hue(object):
             return self.light_modelid
 
 
-#print(Light(1, h).name)
+h = hue("http://192.168.178.75", "O4qAaBl9LaXonrNlAu0Pzei3ianWAJuUzYuZpC2I")
+
+#print(Light(1, h).brightness)

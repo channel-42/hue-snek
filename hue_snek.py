365e481de47b136cd7cb04723f235c2068f1c4ae
channel-42 <info@devls.de>
2021-03-18 20:08:29 +0100

    Formatting code and fixing typos

--- hue_snek.py
+++ hue_snek.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 '''
-Made as a "practice-project" with the help of Phue by  Nathanaël Lécaudé
-A small Hue Api Library for python
-
-
 hue-snek By Channel-42
+
+"practice-project" inspired by Phue (Nathanaël Lécaudé)
+A small Hue Api Library for python
 '''
 
 import json
 import urllib3
 
-#urllib Pool Manager is needed for http requests
+# urllib Pool Manager is needed for http requests
 http = urllib3.PoolManager()
 
 
@@ -18,6 +17,7 @@ class Light(object):
     """Light.
     Light object
     """
+
     def __init__(self, light_id, bridge):
         self.bridge = bridge
         self.light_id = light_id
@@ -30,28 +30,31 @@ class Light(object):
 
     def __repr__(self):
         """__repr__.
-        chenges the way how instace is represented
+        Changes the way an instance is represented
         """
-        return f'<{self.__class__.__module__}.{self.__class__.__name__} object "{self.name}"'
+        return (f'<{self.__class__.__module__}.{self.__class__.__name__}>'
+                f' object "{self.name}"')
 
     def get(self, *args):
         """get.
-        Passthrough function for getting light info
+        Pass through function for getting light info
         Args:
-            args: (str type) e.g. "name", "on", "bri", etc. See Hue Api for all options
+            args: (str type) e.g. "name", "on", "bri", etc.
+            See Hue Api for all options
         """
-        #linking to get_light function for internal class use
+        # linking to get_light function for internal class use
         return self.bridge.get_light(self.light_id, *args)
 
     def set(self, *args):
         """set.
-        Passtrough function to set light properties
+        Pass trough function to set light properties
         Args:
-            args: (str type) e.g. "on", "bri", "hue" (see Hue Api for all options)
+            args: (str type) e.g. "on", "bri", "hue"
+            See Hue Api for all options
         """
         return self.bridge.set_light(self.light_id, *args)
 
-    #get and set properties of light object (name, brightness, etc.)
+    # get and set properties of light object (name, brightness, etc.)
 
     @property
     def name(self):
@@ -104,6 +107,7 @@ class Hue(object):
     """Hue.
     Main bridge object
     """
+
     def __init__(self, ip=None, user=None):
         self.ip = ip
         self.user = user
@@ -124,26 +128,27 @@ class Hue(object):
 
     def checkup(self):
         """checkup.
-        Checks connection to the bridge by doing a http request and then seeing if the API throws any error
+        Checks connection to the bridge by making
+        a http request and then seeing if the API throws any error
         """
-        #basic up check and username check
+        # basic connection and username check
         try:
             api = http.request('GET', self.address)
 
-            #check for http connection
+            # check for http connection
             if api.status != 200:
                 return 1
 
             js = json.loads(api.data.decode('utf-8'))
 
-            #check for api error (eg wrong username)
+            # check for api error (eg wrong username)
             try:
                 if js[0]['error']:
                     return js[0]['error']['description']
-            except:
+            except Exception:
                 pass
                 return 0
-        except:
+        except Exception:
             return 1
 
     def req(self, mode='GET', address=None, data=None):
@@ -154,23 +159,24 @@ class Hue(object):
             address: The URL address of the request
             data: The data sent to the API. Only used if mode is 'PUT'.
         """
-        #main request function for GET and PUT
+
+        # main request function for GET and PUT
         if address is None:
-            address = self.address  #use standard adress (see __init__)
+            address = self.address  # use standard address (see __init__)
         try:
-            #get request
+            # get request
             if mode == 'GET':
                 ret = json.loads(
                     http.request(mode, address).data.decode('utf-8)'))
                 return dict(ret)
-            #put request (body and header needed for urllib3)
+            # put request (body and header needed for urllib3)
             if (mode == 'POST') or (mode == 'PUT'):
                 http.request(mode,
                              address,
                              body=data,
                              headers={'Content-Type': 'application/json'})
                 return 0
-        except:
+        except Exception:
             return 1
 
     def get_light(self, light_id=None, param=None):
@@ -179,19 +185,21 @@ class Hue(object):
 
         Args:
             light_id: Id of a light
-            param: The Parameter that should be returned (e.g. "name", "on"). Leave unset if everything should be returned
+            param: The Parameter that should be returned (e.g. "name", "on").
+            Leave unset if everything should be returned
         """
-        #get light info
+        # get light info
         if light_id is None:
             return 1
 
-        lid = str(light_id)  #make sure light id is string
-        state = self.req('GET', f'{self.address}lights/{lid}')  #make request
+        lid = str(light_id)  # make sure light id is string
+        state = self.req('GET', f'{self.address}lights/{lid}')  # make request
 
         if param is None:
-            return state  #return whole light state page
+            return state  # return whole light state page
 
-        #check if parameter is in main body or in state subbody (see json structure)
+        # check if parameter is in main body
+        # or in state sub-body (see json structure)
         if param in ['name', 'modelid', 'type', 'productname', 'productid']:
             return state[param]
         else:
@@ -199,18 +207,20 @@ class Hue(object):
 
     def set_light(self, light_id=None, param=None, value=None):
         """set_light.
-        Sets a value of a light 
+        Sets a value of a light
         Args:
             light_id: Target light's id
             param: Target parameter (see API for options)
             value: Desired value
         """
-        #set light's parameter value based on light id using the req function
+
+        # set light's parameter value based on light id using the req function
         if (light_id is None) or (param is None) or (value is None):
             return 1
 
         data = f'{{"{param}":{value}}}'
-        lid = str(light_id)  #make sure light_id is string
+        # make sure light_id is string
+        lid = str(light_id)
         self.req('PUT', f'{self.address}lights/{lid}/state', data)
         return 0
 
@@ -218,9 +228,12 @@ class Hue(object):
         """get_lights.
         Gets all lights listed in the API
         Args:
-            mode: The infomration the function returns (str type: obj, id, name, modelid)
+            mode: The information the function returns
+            (str type: obj, id, name, model-id)
         """
-        #gets all lights based on selected mode (standard is object mode) and returns dict
+
+        # gets all lights based on selected mode (standard is object mode)
+        # and returns dict
         lights = self.req('GET', f'{self.address}lights')
 
         for light, val in lights.items():
@@ -243,19 +256,22 @@ class Hue(object):
         Gets a groups and returns it as dict
         Args:
             group_id: The groups id
-            param: The parameter that should be returned (leave unset if everything should be returned)
+            param: The parameter that should be returned
+            (leave unset if everything should be returned)
         """
-        #gets group info
+
+        # gets group info
         if group_id is None:
             return 1
 
-        gid = str(group_id)  #make sure group id is string
-        state = self.req('GET', f'{self.address}groups/{gid}')  #make request
+        gid = str(group_id)  # make sure group id is string
+        state = self.req('GET', f'{self.address}groups/{gid}')  # make request
 
         if param is None:
-            return state  #return whole group state page
+            return state  # return whole group state page
 
-        #check if parameter is in main body or in state subbody (see json structure)
+        # check if parameter is in main body
+        # or in state sub-body (see json structure)
         if param in ['name', 'colormode', 'type', 'alert', 'lights']:
             return state[param]
         else:
@@ -269,12 +285,13 @@ class Hue(object):
             param: Target parameter
             value: Desired value
         """
-        #set group's parameter value based on light id using the req function
+
+        # set group's parameter value based on light id using the req function
         if (group_id is None) or (param is None) or (value is None):
             return 1
 
         data = f'{{"{param}":{value}}}'
-        gid = str(group_id)  #make sure light_id is string
+        gid = str(group_id)  # make sure light_id is string
         self.req('PUT', f'{self.address}groups/{gid}/action', data)
         return 0
 
@@ -285,7 +302,9 @@ class Hue(object):
         Args:
             mode: Changes return mode (str type: all, name, type)
         """
-        #gets all groups based on selected mode (standard is all mode) and returns dict
+
+        # gets all groups based on selected mode (standard is all mode)
+        # and returns dict
         groups = self.req('GET', f'{self.address}groups')
 
         for group, val in groups.items():
@@ -306,14 +325,16 @@ class Hue(object):
         Args:
             mode: Changes return mode (str type: all, name, group)
         """
-        #gets all scenes based on selected mode (standard is all mode) and returns dict
+
+        # gets all scenes based on selected mode (standard is all mode)
+        # and returns dict
         scenes = self.req('GET', f'{self.address}scenes')
 
         for scene, val in scenes.items():
             self.scene_all[str(scene)] = val
             self.scene_name[str(scene)] = val['name']
             self.scene_group[str(scene)] = val.get(
-                "group")  #using ["group] raises a KeyError
+                "group")  # using ["group] raises a KeyError
 
         if mode == "all":
             return self.scene_all
@@ -324,34 +345,37 @@ class Hue(object):
 
     def set_scene(self, group_id=1, scene_name=None):
         """set_scene.
-        Sets a scene for a specified groub (by id)
+        Sets a scene for a specified group (by id)
 
         Args:
             group_id: Target group's id
             scene_name: Desired scene
         """
-        #sets a scene for specific group based on scene name
+
+        # sets a scene for specific group based on scene name
         scenes = self.get_scenes("name")
 
         if scene_name in scenes.values():
-            #get scene id from given name
+            # get scene id from given name
             scene_id = list(scenes.keys())[list(
                 scenes.values()).index(scene_name)]
             data = f'{{"scene":"{scene_id}"}}'
-            #set scene for group
+            # set scene for group
             self.req("PUT", f'{self.address}groups/{group_id}/action', data)
 
     def get_bridge_info(self):
         """get_bridge_info.
-        Returns bridge's name, id, mac-address, model-id and api-version as dict
+        Returns bridge's name, id, mac-address,
+        model-id and api-version as dict
         """
-        #returns dict with bridge info
+
+        # returns dict with bridge info
         info = self.req('GET', f'{self.address}config')
         params = ['name', 'brideid', 'mac', 'modelid', 'apiversion']
 
         ret = {}
 
-        #only add the above params to the return dict
+        # only add the above params to the return dict
         for param, val in info.items():
             if param in params:
                 ret[param] = val
